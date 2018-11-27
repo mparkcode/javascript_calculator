9138c59bd4ceddaf37c0e32fae33c6ec0e8c66c5
Michael Park <michaelpark@Michaels-MacBook-Pro.local>
2018-11-27 19:43:15 +0000

    Added toggling positive/negative functionality

--- calculator.js
+++ calculator.js
@@ -1,28 +1,44 @@
 let operator;
-let currentNum;
+let currentNum = "0";
 let output = document.getElementById('output')
 output.innerHTML = "0"
 let equationString = "";
 let digitClicked = firstDigitClicked;
 let ZeroDigitClicked = doNothing;
-let operatorClicked = doNothing;
+let operatorClicked = firstOperator;
 let dotPressed = dotPressedAsFirstDigit;
+let togglePositiveNegative = togglePositiveNegativeBeforeEqual;
 let equal = doNothing;
 
 function allClear(){
+    console.log("******************")
+    console.log("function is allClear")
     operator = undefined;
-    currentNum = undefined;
+    currentNum = "0";
     digitClicked = firstDigitClicked;
     ZeroDigitClicked = doNothing;
-    operatorClicked = doNothing;
+    operatorClicked = firstOperator;
     dotPressed = dotPressedAsFirstDigit;
+    togglePositiveNegative = togglePositiveNegativeBeforeEqual;
     output.innerHTML = "0";
     equationString = "";
     equal = doNothing;
 }
 
 function doNothing(num){
+    console.log("******************")
+    console.log("function is doNothing")
     console.log('Do Nothing');
+}
+
+function togglePositiveNegativeBeforeEqual(){
+    currentNum = eval(`0-(${currentNum})`)
+    output.innerHTML = currentNum
+}
+
+function togglePositiveNegativeAfterEqual(){
+    equationString = String(eval(`0-(${equationString})`))
+    output.innerHTML = equationString
 }
 
 //--------------------------------------------------------------Digits
@@ -37,21 +53,25 @@ function logicForFirstDigits(num){
     dotPressed = dotPressedAsAdditionalDigit;
     operatorClicked = firstOperator;
     equal = equalPressed;
+    togglePositiveNegative = togglePositiveNegativeBeforeEqual;
 }
 
 function firstDigitClicked(num){
+    console.log("******************")
     console.log(`function is firstDigitClicked`)
     logicForFirstDigits(num);
 }
 
 function firstDigitClickedAfterEquals(num){
     equationString = ""
+    console.log("******************")
     console.log(`function is firstDigitClickedAfterEquals`)
     logicForFirstDigits(num);
 }
 
 function additionalDigitClicked(num){
     currentNum += String(num);
+    console.log("******************")
     console.log(`function is secondDigitClicked`)
     console.log(`current number is ${currentNum}`);
     console.log(`equation string is ${equationString}`);
@@ -63,6 +83,7 @@ function additionalDigitClicked(num){
 
 function dotPressedAsFirstDigit(){
     currentNum = "0."
+    console.log("******************")
     console.log(`function is dotPressedAsFirstDigit`)
     console.log(`current number is ${currentNum}`);
     output.innerHTML=currentNum;
@@ -70,11 +91,13 @@ function dotPressedAsFirstDigit(){
     digitClicked = additionalDigitClicked;
     ZeroDigitClicked = additionalDigitClicked;
     operatorClicked = firstOperator;
+    equal = equalPressed;
 }
 
 function dotPressedAsAdditionalDigit(){
     currentNum += "."
-    console.log(`function is dotPressedAsSecondDigit`)
+    console.log("******************")
+    console.log(`function is dotPressedAsAdditionalDigit`)
     console.log(`current number is ${currentNum}`);
     output.innerHTML=currentNum;
     dotPressed = doNothing;
@@ -84,6 +107,7 @@ function dotPressedAsAdditionalDigit(){
 
 
 function firstOperator(str){
+    console.log("******************")
     console.log(`function is firstOperator`)
     console.log(`first operator:${str} clicked`);
     operator = str;
@@ -101,10 +125,12 @@ function firstOperator(str){
     ZeroDigitClicked = doNothing;
     operatorClicked = secondOperatorClicked;
     dotPressed = dotPressedAsFirstDigit;
-    equal = doNothing;
+    equal = equalPressedAfterOperator;
+    togglePositiveNegative = togglePositiveNegativeBeforeEqual;
 }
 
 function secondOperatorClicked(str){
+    console.log("******************")
     console.log(`function is secondOperatorClicked`)
     console.log(`second operator:${str} clicked`);
     operator = str;
@@ -114,10 +140,8 @@ function secondOperatorClicked(str){
 
 //--------------------------------------------------------------Equals
 
-
-function equalPressed(){
-    console.log("function is equalPressed")
-    equationString = equationString.concat(currentNum)
+function logicForEqual(){
+    console.log("******************")
     console.log(`equation string is ${equationString}`);
     total = eval(equationString);
     output.innerHTML = total;
@@ -126,4 +150,57 @@ function equalPressed(){
     console.log(`total is ${total}`)
     console.log(`equation string is ${equationString}`)
     digitClicked = firstDigitClickedAfterEquals;
+    operatorClicked = firstOperator;
+    togglePositiveNegative = togglePositiveNegativeAfterEqual;
+}
+
+
+let numberInMemory;
+let numberInMemoryForMultipleEquals;
+let operatorInMemory;
+
+function equalPressed(){
+    console.log("******************")
+    console.log("function is equalPressed");
+    numberInMemory = equationString.substring(0,equationString.length-1);
+    numberInMemoryForMultipleEquals = currentNum;
+    operatorInMemory = equationString.substring(equationString.length-1);
+    console.log(`current num is ${currentNum}`)
+    console.log(`number in memory is ${numberInMemory}`)
+    equationString = equationString.concat(`(${currentNum})`);
+    
+    logicForEqual();
+    // equal = equalPressedAfterOperatorMultiple;
+    equal = equalPressedAfterEqual
+}
+
+function equalPressedAfterEqual(){
+    console.log("******************")
+    console.log("function is equalPressedAfterEqual");
+    console.log(`equation string is ${equationString}`)
+    numberInMemory = equationString;
+    equationString = equationString.concat(operatorInMemory, `(${numberInMemoryForMultipleEquals})`);
+    logicForEqual()
+}
+
+
+
+
+function equalPressedAfterOperator(){
+    console.log("******************")
+    console.log("function is equalPressedAfterOperator");
+    console.log(`equation string is ${equationString}`);
+    numberInMemory = equationString.substring(0,equationString.length-1);
+    operatorInMemory = equationString.substring(equationString.length-1);
+    equationString = equationString.concat(`(${numberInMemory})`);
+    logicForEqual();
+    equal = equalPressedAfterOperatorMultiple;
+}
+
+function equalPressedAfterOperatorMultiple(){
+    console.log("******************")
+    console.log("function is equalPressedAfterOperatorMultiple");
+    equationString = equationString.concat(operatorInMemory, `(${numberInMemory})`);
+    logicForEqual();
+
 }
